7afdb187ca16983cc37ef94be86a51f50b8d85cb
AlexanderBakhvalov <78901931+AlexanderBakhvalov@users.noreply.github.com>
2021-03-30 09:10:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,22 @@
 // Copyright 2021 NNTU-CS
+#include "alg.h"
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  int left = *arr;
+  int right = size;
+  int mid = 0;
+  while(left <= right){
+  mid = (left+right)/2;
+  if (value < arr[mid])
+  right = mid - 1;
+  else
+  if (value > arr[mid])
+  left = mid + 1;
+  else
+  if (value == arr[mid])
+  return value;
+  else if (left > right)
+  continue;
+}
   return 0; // если ничего не найдено
 }
