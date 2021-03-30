d3f21a2d6b6c4061cf711cb1d07fe80ed6a17bbb
AlexanderBakhvalov <78901931+AlexanderBakhvalov@users.noreply.github.com>
2021-03-30 10:56:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,26 +1,26 @@
 // Copyright 2021 NNTU-CS
 #include "alg.h"
+int cbinsearch(int *arr, int size, int value) {
+// поместить сюда реализацию алгоритма
+return 0; // если ничего не найдено
+int left = 0, right = size - 1, c = 0;
+while (left < right) {
+int m = (left + right) / 2;
+if (arr[m] < value)
+left = m + 1;
+else
+right = m;
+}
 
-    int left = 0, 
-    int right = size - 1,
-    int с = 0;
-    while (left < right) {
-        int m = (left + right) / 2;
-        if (arr[m] < value)
-            left = m + 1;
-        else
-            right = m;
-    }
+if (arr[left] == value) {
+while (arr[left] == value) {
+c++;
+left++;
+}
+}
 
-    if (arr[left] == value) {
-        while (arr[left] == value) {
-            с++;
-            left++;
-        }
-    }
-
-    if (с)
-        return с;
-    else
-        return 0;
+if (c)
+return c;
+else
+return 0;
 }
